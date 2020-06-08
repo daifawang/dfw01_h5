32320c4d0dad0547ce0bb3b52602441f2060f70e
tianfang <tianfang@log56.com>
2020-06-08 09:50:27 +0800

    任务跳转

--- src/pages/task/taskIndex.vue
+++ src/pages/task/taskIndex.vue
@@ -28,7 +28,7 @@
         </div>
         <div class="task-wrapper" v-if="exclusiveList && exclusiveList.length>0">
           <transition-group appear name="taskList" tag="div">
-            <div v-for="(item, index) in exclusiveList" :key="item.taskId" class="task-li" @click.stop="clickUrl(item.status,item.jumpUrl)">
+            <div v-for="(item, index) in exclusiveList" :key="item.taskId" class="task-li" @click.stop="clickUrl(index,item.status,item.jumpUrl)">
               <div class="task-li-t">
                 <img class="task-t-icon" :src="item.taskHeadImgUrl" />
                 <div class="task-t-div">
@@ -54,7 +54,7 @@
                   <img class="task-invitecard-headimg" :src="item.extra.inviteUserImg" />
                   <span>{{ item.extra.inviteUserName }}给您发了一份邀请...</span>
                 </div>
-                <div class="task-invitecard-bttn" @click.stop="clickUrl(item.status,item.jumpUrl)">立即查看</div>
+                <div class="task-invitecard-bttn" @click.stop="clickUrl(index,item.status,item.jumpUrl)">立即查看</div>
                 <div class="task-invitecard-company">{{ item.extra.inviteCompany }}</div>
               </div>
               <div v-else-if="item.taskType === '3' && item.extra" class="task-li-business">
@@ -109,7 +109,7 @@
         </div>
         <div class="task-wrapper" v-if="newTaskList && newTaskList.length>0">
           <transition-group appear name="taskList" tag="div">
-          <div class="task-wrapper-div" v-for="(newItem,index) in newTaskList" :key="newItem.taskId" @click.stop="clickUrl(newItem.status,newItem.jumpUrl)">
+          <div class="task-wrapper-div" v-for="(newItem,index) in newTaskList" :key="newItem.taskId" @click.stop="clickUrl(index,newItem.status,newItem.jumpUrl)">
             <div class="task-box">
               <div class="task-icon">
                 <img :src="newItem.headImg">
@@ -136,7 +136,7 @@
               <div class="task-btn-div">
                 <div v-if="newItem.status === '2' && showFirstGetCury" @click.stop="closeTip()" class="sj-tag">快去领元宝吧 ×</div>
                 <div @click.stop="clickRightBttn('2',index)" class="task-btn" :class="{guideQuan2:showGuide === 5 && index === 0}">
-                  <div :class="{'to-do':newItem.status=='0','doing':newItem.status=='1','done':newItem.status=='2',}">{{newItem.taskAction}}</div>
+                  <div :class="{'to-do':newItem.status=='0' || newItem.status=='1','doing':newItem.status=='2','done':newItem.status=='3',}">{{newItem.taskAction}}</div>
                 </div>
                 <div v-if="newItem.signCardText" class="task-btn-text">
                   <div>{{newItem.signCardText}}</div>
@@ -192,7 +192,7 @@
         <div class="tasks-title">每日任务</div>
         <div class="task-wrapper" v-if="dailyTaskList && dailyTaskList.length>0">
           <transition-group appear name="taskList" tag="div">
-          <div class="task-wrapper-div" v-for="(dailyItem,index) in dailyTaskList" :key="dailyItem.taskId" @click.stop="clickUrl(dailyItem.status,dailyItem.jumpUrl)">
+          <div class="task-wrapper-div" v-for="(dailyItem,index) in dailyTaskList" :key="dailyItem.taskId" @click.stop="clickUrl(index,dailyItem.status,dailyItem.jumpUrl)">
             <div class="task-box">
               <div class="task-icon">
                 <img :src="dailyItem.headImg">
@@ -379,7 +379,7 @@ export default {
       setTimeout(() => {
         this.loadingFlag = false;
         this.exclusiveList = this.exclusiveList1;
-        this.newTaskList = this.newTaskList1;
+        // this.newTaskList = this.newTaskList1;
         this.showFirstGetCury = true;
         // this.exclusiveFlag = '1'
         // this.newTaskFlag = '1';
@@ -502,6 +502,8 @@ export default {
           let _obj = this.daliyTaskList[index];
           this.daliyTaskList.splice(index, 1);
           this.daliyTaskList.push(_obj);
+          this.daliyTaskList[index].taskAction="已完成";
+          this.daliyTaskList[index].status="2";
         } else if(type == '2'){
           this.newTaskList.splice(index, 1);
           if(this.newTaskList.length === 0){
@@ -510,41 +512,83 @@ export default {
         }
       }, 4000)
     },
+    // 任务按钮
     clickRightBttn(type,index){
+        console.log('------任务按钮-');
       console.log(type,index);
       if(this.showGuide && this.showGuide >0){
         return;
       }
       let status = type === '1' ? this.exclusiveList[index].status : type === '2' ? this.newTaskList[index].status : this.dailyTaskList[index].status;
       let _url = type === '1' ? this.exclusiveList[index].jumpUrl : type === '2' ? this.newTaskList[index].jumpUrl : this.dailyTaskList[index].jumpUrl;
+      let taskId = type === '1' ? this.exclusiveList[index].taskId : type === '2' ? this.newTaskList[index].taskId : this.dailyTaskList[index].taskId;
       if(status === '2'){ //领取元宝
-        //此次还需调接口更新状态后走下一步
-        this.getCurrency(type,index);
+        this.getCurrencyData(taskId,type,index);
       } else if (status === '3') { //已领取-置灰点击没反应
         return;
       } else { //跳转具体任务链接
-        this.goTaskUrl(_url);
+        this.goTaskUrl(index,_url);
       }
     },
     // 点击整个栏跳转
-    clickUrl(status,url){
+    clickUrl(index,status,url){
+        console.log('------点击整个栏跳转--------');
       console.log(status,url);
       if(this.showGuide && this.showGuide >0){
         return;
       }
       if(status !== '2' && status !== '3'){
-         this.goTaskUrl(url);
+         this.goTaskUrl(index,url);
       }
     },
-    goTaskUrl(jump_url){
+    goTaskUrl(index,jump_url){
       console.log('jump_url>>>>'+jump_url);
       if(jump_url){
-        if(jump_url.indexOf('?') < 0 ){
-          window.location.href=`${jump_url}?ttt=${new Date().getTime()}&NEW_WVW_HYB`;
-        }else{
-          window.location.href=`${jump_url}&ttt=${new Date().getTime()}&NEW_WVW_HYB`;
-        }
+          if (jump_url =='-1') {
+            // 跳转刷一刷tab
+            AppJsBridge.appBackMainTab('REFRESH');
+          } else if (jump_url =='-2') {
+            //   跳转刷一刷视频详情 传帖子id
+            let tid = this.dailyTaskList[index].jumpNote;
+            console.log(' 跳转刷一刷视频详情 传帖子id===='+tid);
+            AppJsBridge.goVideoTopicDetail(tid);
+          }else if(jump_url =='-3'){
+            //   跳转任务视频详情
+            let id = this.newTaskList[index].taskId;
+            console.log('跳转任务视频详情id===='+id);
+            this.getVideoInfo(id);
+          }else{
+              if(jump_url.indexOf('?') < 0 ){
+                window.location.href=`${jump_url}?ttt=${new Date().getTime()}&NEW_WVW_HYB`;
+              }else{
+                window.location.href=`${jump_url}&ttt=${new Date().getTime()}&NEW_WVW_HYB`;
+              }
+          }
       }
+    },
+    // 获取视频信息接口
+    getVideoInfo(taskId){
+        AppJsBridge.initSignData({taskId:taskId}, 954004, param => {
+            this.$http({
+            apiType: "2",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            url: "/gateway.do",
+            data: param
+            })
+            .then(res => {
+                console.log(res);
+                if (res.reCode == "0") {
+                    AppJsBridge.taskOpenVideoDetails(res.result[0]);
+                } else {
+                    this.$toast(res.reInfo);
+                }
+            })
+            .catch(e => {
+                console.log(e);
+            });
+      });
     },
     closeTip(){
       this.showFirstGetCury = false;
@@ -663,6 +707,32 @@ export default {
           this.loadingFlag = false;
           this.dailyTaskFlag = '1';
         });
+      });
+    },
+    // 领取元宝接口
+    getCurrencyData(taskId,type,index){
+        // taskType  0 专属  1 新手  2每日
+        console.log('taskId:'+taskId+',type:'+type+',index:'+index);
+        AppJsBridge.initSignData({taskId:taskId,taskType:Number(type)-1+''}, 954011, param => {
+            this.$http({
+            apiType: "2",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            url: "/gateway.do",
+            data: param
+            })
+            .then(res => {
+                console.log(res);
+                if (res.reCode == "0") {
+                    this.getCurrency(type,index);
+                } else {
+                    this.$toast(res.reInfo);
+                }
+            })
+            .catch(e => {
+                console.log(e);
+            });
       });
     }
   }
