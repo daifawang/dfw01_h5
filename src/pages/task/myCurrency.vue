3173ee3db539c0d511549d8c6d20af37a32fd31c
tianfang <tianfang@log56.com>
2020-06-28 15:46:59 +0800

    maxHeight

--- src/pages/task/myCurrency.vue
+++ src/pages/task/myCurrency.vue
@@ -39,7 +39,7 @@
                 </div>
             </div>
             <div :style="{height:topBoxHeight+31+'px'}"></div>
-            <div class="coin-detail" id="coinDetail" :style="{minHeight:boxHeight-topBoxHeight+'px'}" ref="coinDetail">
+            <div class="coin-detail" id="coinDetail" :style="{'maxHeight':coinDetail.length < 10 ? boxHeight-topBoxHeight+'px' : 0}" ref="coinDetail">
                 <van-loading v-if="showLoading" size="24px" vertical>加载中...</van-loading>
                 <div v-if="coinDetail.length>0">
                     <div v-for="(item,index) in coinDetail" :key="index" class="coin-detail-wrapper">
